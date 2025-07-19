6f0d25ab7af22e2a3babd11675ee413dab74cb44
Ahmed Elsherif <gleader21@gmail.com>
2025-07-19 23:41:42 +0300

    just main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, APIRouter, File, UploadFile, HTTPException, Header
 from escpos.printer import Network
-from app.schemas import UploadPng
 import tempfile
 import uvicorn
+from pydantic import BaseModel
+from typing import Optional
+class UploadPng(BaseModel):
+    filename: str
+    size: int
+    target_printer_ip: str
+    printed: bool
+    error: Optional[str] = None
 
 app = FastAPI()
 router = APIRouter()
@@ -54,7 +61,7 @@ app.include_router(router, prefix="/pos-printers")
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=False)
+        uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False)
     except Exception as e:
         print("Error starting server:", e)
     input("Press Enter to exit...")
