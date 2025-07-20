f37615e4f25e23b96c538d3313c4a555935d701f
Ahmed Elsherif <gleader21@gmail.com>
2025-07-20 05:08:01 +0300

    bat donot auto close

--- install_and_run_service.bat
+++ install_and_run_service.bat
@@ -8,3 +8,7 @@ REM Install Elnotah POS Printers Server service
 nssm install "Elnotah POS Printers Server" "C:\Users\Ahmed Elsherif\pos-fastapi\build\Elnotah POS Printers Server\launcher.exe"
 REM Start the service
 nssm start "Elnotah POS Printers Server"
+
+echo.
+echo Press any key to close...
+pause >nul
