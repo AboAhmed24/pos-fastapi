2b830ae92a656f11d30f0b7ecf1cbea53686141a
Ahmed Elsherif <gleader21@gmail.com>
2025-07-20 04:11:07 +0300

    worked perfect

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from cx_Freeze import setup, Executable
 setup(
     name="pos-fastapi",
     version="0.1",
-    executables=[Executable("launcher.py", base="Console")]
+    executables=[Executable("launcher.py", base="Console")],
+    options={
+        "build_exe": {
+            "packages": ["app", "uvicorn", "uvicorn.loops", "uvicorn.protocols", "uvicorn.middleware", "uvicorn.supervisors", "websockets", "anyio", "watchfiles"],
+            "includes": ["uvicorn.loops.auto"]
+        }
+    }
 )
