268832d65e6fbdb81bcd36c2dbc66bf5dd14ce39
Ahmed Elsherif <gleader21@gmail.com>
2025-07-20 18:51:31 +0300

    works perfect with coping key and cert manualy

--- install_and_run_service.bat
+++ install_and_run_service.bat
@@ -5,7 +5,7 @@ nssm stop "Elnotah POS Printers Server"
 nssm remove "Elnotah POS Printers Server" confirm
 
 REM Install Elnotah POS Printers Server service
-nssm install "Elnotah POS Printers Server" "C:\Users\Ahmed Elsherif\pos-fastapi\build\Elnotah POS Printers Server\launcher.exe"
+nssm install "Elnotah POS Printers Server" "C:\Elnotah POS Printers Server\launcher.exe"
 REM Start the service
 nssm start "Elnotah POS Printers Server"
 
--- launcher.py
+++ launcher.py
@@ -1,10 +1,25 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=5000,
-        ssl_keyfile="key.pem",
-        ssl_certfile="cert.pem",
-    )
+    try:
+        import os, sys
+        if getattr(sys, 'frozen', False):
+            base_dir = sys._MEIPASS if hasattr(sys, '_MEIPASS') else os.path.dirname(sys.executable)
+        else:
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+        uvicorn.run(
+            "app.main:app",
+            host="0.0.0.0",
+            port=5000,
+            ssl_keyfile=os.path.join(base_dir, "key.pem"),
+            ssl_certfile=os.path.join(base_dir, "cert.pem"),
+        )
+    except Exception as e:
+        import traceback
+        print("An error occurred:")
+        traceback.print_exc()
+        print("Current directory:", os.getcwd())
+        print("key.pem path:", os.path.join(base_dir, "key.pem"))
+        print("cert.pem path:", os.path.join(base_dir, "cert.pem"))
+        print(e)
+    input("Press Enter to exit...")
