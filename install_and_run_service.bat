8db8b902329dbd45d43caa745d067607c76a8ca3
Ahmed Elsherif <gleader21@gmail.com>
2025-07-20 04:47:49 +0300

    stop service at first

--- install_and_run_service.bat
+++ install_and_run_service.bat
@@ -1,5 +1,9 @@
 @echo off
 
+REM Stop and remove existing service if present
+nssm stop "Elnotah POS Printers Server"
+nssm remove "Elnotah POS Printers Server" confirm
+
 REM Install Elnotah POS Printers Server service
 nssm install "Elnotah POS Printers Server" "C:\Users\Ahmed Elsherif\pos-fastapi\build\Elnotah POS Printers Server\launcher.exe"
 REM Start the service
